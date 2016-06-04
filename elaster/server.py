c88e5603a808419886bf458b2a86f15e3abf8dee
Anirban Roy Das <anirban.nick@gmail.com>
2016-06-04 07:55:33 +0530

    added elasticsearch connection creationg method, data indexing method

--- elaster/server.py
+++ elaster/server.py
@@ -1,37 +1,120 @@
+""" 
+The server module starts the elaster server and initiates the elasticsearch connection. 
+It also takes addtional option example parameter to initiate the example indices. 
+
+"""
+
+
+
+
 import tornado.ioloop
 import tornado.httpserver
 import tornado.web
 from tornado.options import define, options
 import logging
 import os.path
+from elasticsearch import Elasticsearch 
 
 from urls import urls
 from settings import settings
+from elaster.apps.elasticsearch.searchEngine import ElasticsearchClient
 
 
 define("port", default=9091, help="run on the given port", type=int)
-define("example", default='webapp', help='the example app to run in elaster server', type=str)
+define("example", default=None, help='the example app to run in elaster server', type=str)
+define("data-path", default=None, help='the path to data to be indexed into elasticsearch for searching over it', type=str)
 
 
 LOGGER = logging.getLogger(__name__)
 
 
 class Application(tornado.web.Application):
+    """ 
+    Application Class initiates the elaster server app with proper settings and providing methods to create
+    the elasticsearch connection and start the tornado server. 
 
-    def __init__(self, example):
-        tpath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'examples', example))
-        spath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'examples', example, 'static'))
+    It subclasses tornado.web.Application.
 
-        settings['template_path'] = tpath
-        settings['static_path'] = spath
+    """
+
+
+    def __init__(self, example=None, data_path=None):
+        """ 
+        Initialization method for the application. 
+
+        :param  example:   The example to be used.
+        :type   example:    string or default is None 
+
+        """ 
+
+        self.es_conn = Elasticsearch()
+
+        if example is not None:
+            tpath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'examples', example))
+            spath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'examples', example, 'static'))
+            settings['template_path'] = tpath
+            settings['static_path'] = spath
+
+            # create the example dataset index with the created ElasticSearch conn
+            self._createExampleDataIndex(self.es_conn)
+        
+        else: 
+            # create the dataset index from the give path 
+            self._createDataIndex(data_path)
 
         tornado.web.Application.__init__(self, urls, **settings)
 
 
+    def _createExampleDataIndex(self, conn): 
+        """ 
+        Method to create the dataset for using in the example apps. 
+
+        :param  conn:   The elasticsearch connection object 
+        :type   conn:   elasticsearch.Elasticsearch 
+
+        """ 
+
+        # //Todo 
+    
+
+
+    def _createDataIndex(self, path):
+        """ 
+        Method to create the Data Index into Elasticsearch to search over it from the give path.
+
+        It checks the path, and if its a file, it indexes it directly, and if its a dirctory, it indexes
+        all the individual files inside the directory to upto 1 level down. So, all the data should be in 
+        the same directory as the path.
+
+        :param  path:   the directory path to data, or the file itself to be indexed (the files should be JSON files.)
+        :type   path:   string
+
+        """ 
+
+        # //Todos
+        # 
+        with open(path, 'rb') as f:
+            data = f.read()
+            index = path 
+        
+         
+        ElasticsearchClient.prepare_index(index)
+        ElasticsearchClient.index_bulk(index, data)
+    
+
+
+
 
 def main():
+    """ 
+    The main methos starts the server by accepting the Application object.
+    It starts the server at the given port extracted from options.port otherwise it
+    takes a default value of 9091.
+
+    """ 
+
     tornado.options.parse_command_line()
-    app = Application(options.example)
+    app = Application(example=options.example)
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
 
